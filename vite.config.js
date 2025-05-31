2654e93c0e11fb83b9e49b468d030afc48d8a38a
MidiKang <midikang@t3ex-group.com>
2025-06-01 07:17:07 +0800

    修复Vue模块加载问题，使用CDN源

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: '/panio/', // 设置基础路径为仓库名
+  resolve: {
+    alias: {
+      'vue': 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+    }
+  },
   build: {
     outDir: 'dist', // 输出目录
     assetsDir: 'assets', // 静态资源目录
